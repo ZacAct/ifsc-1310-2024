f3f5e5ea3209745400bb6eaac97b3cb62e7e62b8
ZacAct <158017641+ZacAct@users.noreply.github.com>
2024-05-03 20:47:49 +0000

    fixed segway footer, added reflection

---
+++ slick/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slidesToShow: 1,
+      cssEase: 'linear',
+      autoplay: false,
+      autoplaySpeed: 2000
+    });
+
+
+});
